7964d6922d6a94720ee2dbae78be2f0e9a4e7484
Eziones <romaincastro@gmail.com>
2023-10-23 21:20:26 +0200

    L2 over

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: "Shoes",
+            description: "Objet à mettre aux pieds qui nous permet de sortir"
+        }
+    }
+})
